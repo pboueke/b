a81de40da50c350025afa3aedeffe7810e95fc67
Pedro Boueke <pedro.boueke@bigdatacorp.com.br>
2015-12-22 17:35:23 -0200

    Visual changes and cleaning.

--- public/projects/threejs/cg02/cubes.js
+++ public/projects/threejs/cg02/cubes.js
@@ -4,7 +4,11 @@
 
 //global
 var container, camera, controls, scene, renderer, light;
+
+//objects
 var objects = [], plane;
+
+//auxiliar and status
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2(),
 offset = new THREE.Vector3(),
@@ -14,7 +18,7 @@ var rotationmsg;
 
 //main
 start();
-animate();
+render();
 
 function start() {
     //initializes most variables
@@ -25,6 +29,7 @@ function start() {
     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
     camera.position.z = 1000;
 
+    // controls
     controls = new THREE.TrackballControls( camera );
     controls.rotateSpeed = 10;
     controls.zoomSpeed = 1.2;
@@ -34,9 +39,11 @@ function start() {
     controls.staticMoving = true;
     controls.dynamicDampingFactor = 0.2;
 
+    //scene
     scene = new THREE.Scene();
     scene.add( new THREE.AmbientLight( 0x505050 ) );
 
+    //lighting
     light = new THREE.SpotLight( 0xffffff, 1.0 );
     light.position.set( 0, 500, 2000 );
     light.castShadow = true;
@@ -62,8 +69,7 @@ function start() {
     renderer.sortObjects = false;
     container.appendChild( renderer.domElement );
 
-    //TODO: skybox
-    /*
+    //TODO: fix skybox
     var urlPrefix = "clouds/";
     var urls = [ urlPrefix + "Skybox-Right.bmp", urlPrefix + "Skybox-Left.bmp",
         urlPrefix + "Skybox-Top.bmp", urlPrefix + "Skybox-Bottom.bmp",
@@ -82,7 +88,7 @@ function start() {
     }),
     skyboxMesh = new THREE.Mesh( new THREE.BoxGeometry( 100000, 100000, 100000, 1, 1, 1, null, true ), material );
     scene.add( skyboxMesh );
-    */
+    
 
     // add event listeners
     renderer.domElement.addEventListener( 'mousemove', onDocumentMouseMove, false );
@@ -90,7 +96,6 @@ function start() {
     renderer.domElement.addEventListener( 'mouseup', onDocumentMouseUp, false );
     window.addEventListener( 'keydown', onDocumentKeyPressed, false );
     window.addEventListener( 'resize', onWindowResize, false );
-
 }
 
 var random_color = function () {
@@ -98,6 +103,22 @@ var random_color = function () {
     "use strict";
     return '#' + Math.floor(Math.random() * 16777215).toString(16);
 };
+
+function openControls() {
+    swal({   title: "Controls!",   text: "'c' - create a new cube <br>'x' - delete hovered cube <br> 'space bar' - toggle rotation mode <br>",   html: true });
+}
+
+function toggleMode() {
+    if (ROTATION == false) {
+        ROTATION = true;
+        document.getElementById("displayMode").value = "Rotation Mode   "; 
+        return;
+    } else {
+        ROTATION = false;
+        document.getElementById("displayMode").value = "Translation Mode";
+        return;
+    }
+}
 
 function onWindowResize() {
     // takes careof aspect ratioon resize
@@ -108,20 +129,17 @@ function onWindowResize() {
 }
 
 function onDocumentMouseMove( event ) {
-
     event.preventDefault();
-
     // clears the scene from any sphere used for the vizualization of the trackballcontroll
     var aux_object = scene.getObjectByName("rotSphere");
     while (aux_object !== undefined) {
         aux_object = scene.getObjectByName("rotSphere");
         scene.remove(aux_object);
     }
-
     mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;  //normalize mouse coordinates
     mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-    raycaster.setFromCamera( mouse, camera ); //cast ray from camera based on the position of the mouse
-    if ( SELECTED && !ROTATION) { //gets intersections with the plane 
+    raycaster.setFromCamera( mouse, camera );                 //cast ray from camera based on the position of the mouse
+    if ( SELECTED && !ROTATION) {                             //gets intersections with the plane 
         var intersects = raycaster.intersectObject( plane );
         if ( intersects.length > 0 ) {
             SELECTED.position.copy( intersects[0].point);
@@ -154,12 +172,11 @@ function onDocumentMouseMove( event ) {
             //process rotation
             deltaX = event.clientX - startPoint.x;
             deltaY = event.clientY - startPoint.y;
-            handleRotation();
+            rotate();
             startPoint.x = event.clientX;
             startPoint.y = event.clientY;
-
-
         }
+    
 }
 
 function onDocumentMouseDown( event ) {
@@ -178,7 +195,7 @@ function onDocumentMouseDown( event ) {
             x: event.clientX,
             y: event.clientY
         };
-        rotateStartPoint = rotateEndPoint = projectOnTrackball(0, 0);
+        rotateStartPoint = rotateEndPoint = projTrackball(0, 0);
     }
     if ( ROTATION ){
         ROTATING = true;
@@ -201,14 +218,12 @@ function onDocumentMouseUp( event ) {
 function onDocumentKeyPressed( event ) {
     //handles all the keydown events
     if(event.which == 32){ //toggles between rotation and translationwith the space bar
-        if (ROTATION == false) { //toggle rotate mode
-            ROTATION = true;
-            //container.children[2].innerHTML = 'ROTATE MODE'  
-            return;
-        } else {
-            ROTATION = false;
-            //container.children[2].innerHTML = 'MOVE MODE'; 
-            return;
+        toggleMode();
+    }  else if ( event.which == 88) { // handles deletation of cubeswith x
+        raycaster.setFromCamera( mouse, camera );
+        var intersection = raycaster.intersectObjects( objects );
+        if (intersection.length > 0){
+            scene.remove(scene.getObjectById(intersection[0].object.id));
         }
     } else if ( event.which == 67) {  //  handles creation of new cubes with c
         var min = 30,
@@ -225,18 +240,13 @@ function onDocumentKeyPressed( event ) {
         cube.position.z = intersection[0].point.z;
         cube.castShadow = true;
         cube.receiveShadow = true;
+        cube.name = "cube";
         scene.add( cube );
         objects.push( cube );
-    } else if ( event.which == 88) { // handles deletation of cubeswith x
-        raycaster.setFromCamera( mouse, camera );
-        var intersection = raycaster.intersectObjects( objects );
-        if (intersection.length > 0){
-            scene.remove(scene.getObjectById(intersection[0].object.id));
-        }
     }
 }
 
-function projectOnTrackball(touchX, touchY) {
+function projTrackball(touchX, touchY) {
     var mouseOnBall = new THREE.Vector3();
     mouseOnBall.set(
         clamp(touchX / (window.innerWidth/2), -1, 1), clamp(-touchY / (window.innerHeight/2), -1, 1),
@@ -252,43 +262,38 @@ function projectOnTrackball(touchX, touchY) {
     return mouseOnBall;
 }
 
-var handleRotation = function() {
-        rotateEndPoint = projectOnTrackball(deltaX, deltaY);
-        var rotateQuaternion = rotateMatrix(rotateStartPoint, rotateEndPoint);
-        curQuaternion = SELECTED.quaternion;
-        curQuaternion.multiplyQuaternions(rotateQuaternion, curQuaternion);
-        curQuaternion.normalize();
-        SELECTED.setRotationFromQuaternion(curQuaternion);
-        rotateEndPoint = rotateStartPoint;
-    };
+var rotate = function() {
+    rotateEndPoint = projTrackball(deltaX, deltaY);
+    var rotatingQuartenion = rotateMatrix(rotateStartPoint, rotateEndPoint);
+    curQuaternion = SELECTED.quaternion;
+    curQuaternion.multiplyQuaternions(rotatingQuartenion, curQuaternion);
+    curQuaternion.normalize();
+    SELECTED.setRotationFromQuaternion(curQuaternion);
+    rotateEndPoint = rotateStartPoint;
+};
 
-function rotateMatrix(rotateStart, rotateEnd) {
-        var axis = new THREE.Vector3(),
-            quaternion = new THREE.Quaternion();
-        var angle = Math.acos(rotateStart.dot(rotateEnd) / rotateStart.length() / rotateEnd.length());
+function rotateMatrix(start, end) {
+    //base matrix operation
+    var axis = new THREE.Vector3(),
+        quaternion = new THREE.Quaternion();
+    var angle = Math.acos(start.dot(end) / start.length() / end.length());
 
-        if (angle) {
-            axis.crossVectors(rotateStart, rotateEnd).normalize();
-            angle *= 2;
-            quaternion.setFromAxisAngle(axis, angle);
-        }
-        return quaternion;
+    if (angle) {
+        axis.crossVectors(start, end).normalize();
+        angle *= 2;
+        quaternion.setFromAxisAngle(axis, angle);
     }
+    return quaternion;
+}
 
 function clamp(value, min, max)
 {
     return Math.min(Math.max(value, min), max);
 }
 
-function animate() {
-
-    requestAnimationFrame( animate );
-    render();
-
-}
-
 function render() {
 
+    requestAnimationFrame(render);
     controls.update();
     light.position.set(camera.position.x,camera.position.y,camera.position.z);
     renderer.render( scene, camera );
